24f0b8e0ac619346bf4c6149966014e76749aeb5
laxminarayanvyas <laxminarayanvyas077@gmail.com>
2024-12-05 11:11:43 +0530

    disable box after win

--- script.js
+++ script.js
@@ -12,33 +12,38 @@ const winPatterns = [
 // let gameState = Array(9).fill(false);
 let boxes = document.querySelectorAll(".box");
 let player1 = true;
+let gameOver = false;
 // let won=false;
 
 boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        // box.innerText="hry";
-        //dont add more para if already there is move...
-        if (box.classList.contains('played')) {
-            return;
-        }
-        let paragraph = document.createElement("p");
-        // paragraph.innerText="hi";
-        paragraph.innerText = addMoveToBoard();
-        box.append(paragraph);
-
-        // Mark the box as played by adding the 'played' class
-        box.classList.add('played');
-
-        let sign=checkIfPlayerWon();
-        if(sign!=null){
-            let player= sign=="O"?"Player 1": "Player 2";
-            let resultBoard= document.createElement("h3");
-            resultBoard.innerText=`${player} has won the match`;
-            let resultDiv= document.querySelector("#result");
-            resultDiv.append(resultBoard);
-        }
-    })
+    box.addEventListener("click", handleClick);
 })
+function handleClick(event) {
+    // box.innerText="hry";
+    const box=event.target;
+    //dont add more para if already there is move...
+    if (box.classList.contains('played') || gameOver) {
+        return;
+    }
+    let paragraph = document.createElement("p");
+    // paragraph.innerText="hi";
+    paragraph.innerText = addMoveToBoard();
+    box.append(paragraph);
+
+    // Mark the box as played by adding the 'played' class
+    box.classList.add('played');
+
+    let sign = checkIfPlayerWon();
+    if (sign != null) {
+        let player = sign == "O" ? "Player 1" : "Player 2";
+        let resultBoard = document.createElement("h3");
+        resultBoard.innerText = `${player} has won the match`;
+        let resultDiv = document.querySelector("#result");
+        resultDiv.append(resultBoard);
+        gameOver = true;  // Mark the game as over
+        // disableAllBoxes();
+    }
+}
 const addMoveToBoard = () => {
     if (player1) {
         player1 = false;
@@ -66,3 +71,9 @@ const checkIfPlayerWon = () => {
     return null;
     // console.log(boxes[2]);
 }
+const disableAllBoxes = () => {
+    boxes.forEach((box) => {
+        box.removeEventListener("click", handleClick);  // Remove click listeners
+        // box.classList.add("disabled");  // Add a 'disabled' class for styling (optional)
+    });
+};
